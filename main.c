9b19d94205c1ea64e9a3e36025ca842656d905b4
Saim Khan <saimkhan065@gmail.com>
2023-03-09 15:27:43 -0500

    implemented square number function with pointers

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 #include<stdlib.h>
 
 int strLen(char *pointer); // function declaration
+int square_number(int *const number);
 
 int main() {
     //test inputs
     char str_1[] = "this is a test string";
     char str_2[] = "";
     char str_3[] = " ";
+    const int number_1 = 10;
+    const int number_2 = 0;
+    const int number_3 = 52;
     //print statements
     printf("Length of str_1: %d\n", strLen(str_1)); // pass by reference
     printf("Length of str_2: %d\n", strLen(str_2));
     printf("Length of str_3: %d\n", strLen(str_3));
+    printf("square of number_1: %d", square_number(&number_1));
+    printf("square of number_2: %d", square_number(&number_2));
+    printf("square of number_3: %d", square_number(&number_3));
     return 0;
 }
 
@@ -23,4 +30,8 @@ int strLen(char *pointer) { // function definition
     while(*address_to_str) // while value @ ptr != \0 -> increment address
         *address_to_str++;
     return address_to_str - pointer; // subtract str[last] - str[first]
+}
+
+int square_number(int *const number){
+    return (*number) * (*number);
 }
