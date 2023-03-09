e79639bbc535f25c2fa81a3c39079f85e51e4771
Saim Khan <saimkhan065@gmail.com>
2023-03-09 14:37:35 -0500

    added comments

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-int strLen(char *pointer);
+int strLen(char *pointer); // function declaration
 
 int main() {
+    //test inputs
     char str_1[] = "this is a test string";
     char str_2[] = "";
     char str_3[] = " ";
@@ -17,9 +18,9 @@ int main() {
     return 0;
 }
 
-int strLen(char *pointer) {
-    char *address_to_str = pointer;
-    while(*address_to_str)
+int strLen(char *pointer) { // function definition
+    char *address_to_str = pointer; // pointer to str[0]
+    while(*address_to_str) // while value @ ptr != \0 -> increment address
         *address_to_str++;
-    return address_to_str - pointer;
+    return address_to_str - pointer; // subtract str[last] - str[first]
 }
